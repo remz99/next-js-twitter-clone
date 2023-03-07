e15063b8e958d6b2c2b01ce20c19d6f36f8c9a6d
Remy Flatt <remy.flatt@qotient.com>
2023-03-08 12:33:25 +1300

    Add Prisma ORM

---
+++ prisma/migrations/20230307233144_create_tweets/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
